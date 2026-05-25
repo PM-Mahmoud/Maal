f53a5b2544dbc09bd6dba0e5bd5eb1ce2f9af976
Mahmoud <mahmoud121999@gmail.com>
2026-05-25 11:02:07 +1000

    Fix phone migration format

--- migrations/1748020000000_phone.js
+++ migrations/1748020000000_phone.js
@@ -1,12 +1,9 @@
-'use strict';
-const { pool } = require('../db/pool');
-
-async function up() {
-  await pool.query(`
-    ALTER TABLE users ADD COLUMN IF NOT EXISTS phone VARCHAR(20);
-    ALTER TABLE users ADD COLUMN IF NOT EXISTS phone_verified BOOLEAN DEFAULT FALSE;
-  `);
-  console.log('Migration: phone columns added');
-}
-
-up().catch(err => { console.error(err); process.exit(1); });
+module.exports = {
+  name: 'phone',
+  up: async (client) => {
+    await client.query(`
+      ALTER TABLE users ADD COLUMN IF NOT EXISTS phone VARCHAR(20);
+      ALTER TABLE users ADD COLUMN IF NOT EXISTS phone_verified BOOLEAN DEFAULT FALSE;
+    `);
+  }
+};
